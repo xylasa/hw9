67a8a51ca3a660ba5bc2887639b4ea81905f302a
xyla <xyla@gmail.com>
2023-10-15 21:04:12 +0700

    hw9

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const app = express()
 const port = 3000
 const router = require("./routes")
 const errorHandler = require("./middlewares/errorHandler.js")
+const morgan = require("morgan")
+
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use(router);
+
+// Morgan
+app.use(morgan('dev'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+
 
 // error handling
 app.use(errorHandler)
